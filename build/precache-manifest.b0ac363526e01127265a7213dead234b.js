8507fcddca89bcf3457036c21715c43310df8b2b
Sumit kumar <sumit.k@tyroo.com>
2020-05-23 19:09:41 +0530

    package.json updated

---
+++ build/precache-manifest.b0ac363526e01127265a7213dead234b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f6123aa32cfc63f0ccc2199607e621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1210e887cab3dcb3ab7",
+    "url": "/static/css/main.79707827.chunk.css"
+  },
+  {
+    "revision": "44df277e51c886d361a5",
+    "url": "/static/js/2.776996f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.776996f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1210e887cab3dcb3ab7",
+    "url": "/static/js/main.2aac9834.chunk.js"
+  },
+  {
+    "revision": "253d2bcd21f298c81398",
+    "url": "/static/js/runtime-main.ef00e89d.js"
+  }
+]);
